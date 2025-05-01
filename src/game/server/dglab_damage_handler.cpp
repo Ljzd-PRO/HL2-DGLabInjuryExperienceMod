a2bd3991c189e8cd1e925b5ece686d8e79f9de6b
Ljzd-PRO <me@ljzd.link>
2025-05-02 02:46:06 +0800

    docs: 补充注释

--- src/game/server/dglab_damage_handler.cpp
+++ src/game/server/dglab_damage_handler.cpp
@@ -4,6 +4,8 @@
 #include "npc_metropolice.h"
 #include <future>
 
+// The waveform length is 200ms, and the frequency is set to half of the waveform length (100ms) to ensure output
+// If you need to increase the waveform length, make sure the corresponding frequency allows for at least one output within the waveform length
 std::vector<dglab::Pulse> dglab_damage_handler::pulse_data = {
     {{100, 100, 100, 100}, {100, 100, 100, 100}},
     {{100, 100, 100, 100}, {100, 100, 100, 100}},
@@ -28,6 +30,8 @@ void dglab_damage_handler::HandleDamage(const CTakeDamageInfo& info, const CAI_B
     const float max_health = static_cast<float>(npc.GetMaxHealth());
     const float strength_percentage = min(info.GetDamage(), max_health) / max_health;
 
+    // Based on testing, it's better to complete operations on one channel before moving to another
+    // Otherwise, it might cause the other waveform to not be output
     dglab::client.set_strength_percentage(dglab::Channel::A, strength_percentage);
     dglab::client.add_pulses(dglab::Channel::A, pulse_data);
     dglab::client.set_strength_percentage(dglab::Channel::B, strength_percentage);
